d619fa500165ca8ef3c0c9ae04f6a1eba0f02b5c
moebiusSurfing <moebiussurfing@gmail.com>
2025-10-18 19:12:18 +0200

    .

--- exampleEdgeCannyDetector/src/ofApp.cpp
+++ exampleEdgeCannyDetector/src/ofApp.cpp
@@ -1,23 +1,30 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
-    edgeDetector.setup();
-    img.load("TheDavid.png");
+void ofApp::setup() {
+	auto t = ofGetElapsedTimeMillis();
+
+	edgeDetector.setup();
+	img.load("TheDavid.png");
+
+	ofLogNotice("ofApp") << "Processing time (ms): " << (ofGetElapsedTimeMillis() - t);
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
-
+void ofApp::update() {
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
-    edgeDetector.begin();
-    img.draw(0,0);
-    edgeDetector.end();
+void ofApp::draw() {
+	auto t = ofGetElapsedTimeMillis();
+
+	edgeDetector.begin();
+	img.draw(0, 0);
+	edgeDetector.end();
+
+	if (ofGetFrameNum() % 120 == 0) ofLogNotice("ofApp") << "Processing time (ms): " << (ofGetElapsedTimeMillis() - t);
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::keyPressed(int key) {
 }
